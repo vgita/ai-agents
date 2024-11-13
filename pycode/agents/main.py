7f8f29c1ec7a8ed344d2fe791bcc9c363130a2d3
vgita <22777783+vgita@users.noreply.github.com>
2024-11-13 10:57:03 +0200

    add agent memory

--- pycode/agents/main.py
+++ pycode/agents/main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
 from tools.report import write_report_tool
+from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
 
@@ -19,9 +20,15 @@ prompt = ChatPromptTemplate(
       "Do not make any assumptions about what tables exists "
       "or what columns exist. Instead, use the 'describe_tables' function."
     )),
+    MessagesPlaceholder(variable_name="chat_history"),
     HumanMessagePromptTemplate.from_template("{input}"),
     MessagesPlaceholder(variable_name="agent_scratchpad")
   ]
+)
+
+memory = ConversationBufferMemory(
+  memory_key="chat_history",
+  return_messages=True
 )
 
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
@@ -29,14 +36,18 @@ tools = [run_query_tool, describe_tables_tool, write_report_tool]
 agent = OpenAIFunctionsAgent(
   llm=chat,
   prompt=prompt,
-  tools=tools
+  tools=tools,
 )
 
 agent_executor = AgentExecutor(
   agent=agent,
   verbose=True,
-  tools=tools
+  tools=tools,
+  memory=memory
 )
 
 #agent_executor("How many users have provided a shipping address?")
-agent_executor("Summarize the top 5 most popular products. Write the results to a report file.")
+#agent_executor("Summarize the top 5 most popular products. Write the results to a report file.")
+
+agent_executor("How many orders are there? Write the result in an html report.")
+agent_executor("Repeat the exact same process for users")
