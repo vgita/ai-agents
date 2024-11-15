91e4b19174a3656b0450e04f345ef689a1c7bd57
vgita <22777783+vgita@users.noreply.github.com>
2024-11-15 15:26:33 +0200

    add custom callback handlers

--- pycode/agents/main.py
+++ pycode/agents/main.py
@@ -7,10 +7,14 @@ from dotenv import load_dotenv
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
 from tools.report import write_report_tool
 from langchain.memory import ConversationBufferMemory
+from handlers.chat_model_start_handler import ChatModelStartHandler
 
 load_dotenv()
 
-chat = ChatOpenAI()
+handler = ChatModelStartHandler()
+chat = ChatOpenAI(
+  callbacks=[handler]
+)
 tables = list_tables()
 prompt = ChatPromptTemplate(
   messages=[
@@ -41,7 +45,7 @@ agent = OpenAIFunctionsAgent(
 
 agent_executor = AgentExecutor(
   agent=agent,
-  verbose=True,
+  #verbose=True,
   tools=tools,
   memory=memory
 )
